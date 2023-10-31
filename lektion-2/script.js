1ef5236220230ee70a50ddb696dffbebcd7e7de1
Ben Lundström <ben.lundstrom@arcada.fi>
2023-10-31 11:58:31 +0200

    filerna för lektion 2

---
+++ lektion-2/script.js
@@ -0,0 +1,15 @@
+const namn = "JavaScript";
+const version = 1.0
+
+const str1 = "Hello " + namn + " version " + version
+console.log(str1)
+function hello(){
+    const str2 =`Hello function!`
+    console.log(str2)
+}
+
+function buttonHandler(){
+    console.log("Du trycke på knappen!")
+}
+
+hello();
