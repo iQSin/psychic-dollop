f3801891779895f41bcfe8e9b6ff9918e6043da9
Ben Lundström <ben.lundstrom@arcada.fi>
2023-11-09 10:36:49 +0200

    working solution

--- lektion-4/code-challenge.js
+++ lektion-4/code-challenge.js
@@ -8,6 +8,17 @@ const info = [
 
 info[1].price = 5;
 
+const ulElement = document.getElementById("object-list");
+
 console.log(`En ${info[0].names} kostar ${info[0].price}, vi har ${info[0].stock} st`);
 console.log(`En ${info[1].names} kostar ${info[1].price}, vi har ${info[1].stock} st`);
 console.log(`En ${info[2].names} kostar ${info[2].price}, vi har ${info[2].stock} st`);
+
+for(let i of info)
+{
+    const liElement = document.createElement("li");
+
+    liElement.textContent = `Namn: ${i.names}, Pris: ${i.price}, Antal: ${i.stock}`;
+
+    ulElement.appendChild(liElement);
+}
